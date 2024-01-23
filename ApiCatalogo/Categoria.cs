894a522f574858a0dd7349ae7f46de993ba77557
Matheus Gomes <matheusprgc@gmail.com>
2024-01-22 21:50:12 -0300

    Primeira API

---
+++ ApiCatalogo/Categoria.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.ObjectModel;
+
+namespace ApiCatalogo;
+
+public class Categoria
+{
+
+    public Categoria()
+    {
+       Produtos = new Collection<Produto>();
+    }  
+    public int CatoriaId { get; set; }
+
+    public string? Nome { get; set; }
+
+    public string? ImagemUrl { get; set; }
+
+    public ICollection<Produto>?Produtos { get; set; }
+}
